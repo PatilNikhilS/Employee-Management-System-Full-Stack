2d8fc1ef8d9650277e2c5862259754859a7e7542
Nikhil Suresh Patil <89438683+PatilNikhilS@users.noreply.github.com>
2023-03-25 12:13:31 +0530

    Add files via upload

---
+++ Employee-next-js-ui/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-gray-800">
+      <div className="h-16 px-8 flex items-center"></div>
+    </div>
+  );
+};
+
+export default Footer;
